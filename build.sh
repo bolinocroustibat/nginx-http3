27b60eb50ade5f022f38d6d4281c55a0783981cd
Adrien Carpentier <contact@adriencarpentier.com>
2023-11-22 12:41:03 +0800

    Update build.sh to include --with-http_gzip_static_module for ngx_http_access_module
    See https://stackoverflow.com/questions/46236349/do-i-have-to-turn-on-ngx-http-access-module

--- build.sh
+++ build.sh
@@ -45,7 +45,27 @@ auto/configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx \
 --without-http_upstream_hash_module --without-http_upstream_ip_hash_module \
 --without-http_upstream_keepalive_module --without-http_upstream_least_conn_module \
 --without-http_upstream_random_module --without-http_upstream_zone_module \
---with-openssl=modules/openssl > /dev/null 2>&1
+--with-openssl=modules/openssl > /dev/null 2>&1 \
+--with-http_ssl_module  \
+--with-http_stub_status_module \
+--with-http_realip_module \
+--with-http_auth_request_module \
+--with-http_v2_module \
+--with-http_dav_module \
+--with-http_slice_module \
+--with-threads \
+--with-http_addition_module \
+--with-http_flv_module \
+--with-http_gunzip_module \
+--with-http_gzip_static_module \
+--with-http_mp4_module \
+--with-http_random_index_module \
+--with-http_secure_link_module \
+--with-http_sub_module \
+--with-mail_ssl_module \
+--with-stream_ssl_module \
+--with-stream_ssl_preread_module \
+--with-stream_realip_module \
 make -j$(nproc) > /dev/null 2>&1
 cp objs/nginx ..
 cd ..
